f056554379b4ef3112d5a37e55df13dc65d75258
sik2 <devpang20@gmail.com>
2023-05-02 12:40:43 +0900

    개선된 스위치 추가

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,14 +1,43 @@
 package com.ll.exam.sbb;
 
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    @GetMapping("/sbb")
+    int num = -1;
+
+    @RequestMapping("/plus")
     @ResponseBody
-    public String index() {
-        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    public int showPlus(int a, int b) {
+        return a + b;
+    }
+
+    @RequestMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
+
+    @RequestMapping("/increase")
+    @ResponseBody
+    public int showIncrease() {
+        num++;
+        return num;
+    }
+
+    @RequestMapping("/mbti/{name}")
+    @ResponseBody
+    public String showMbti(@PathVariable  String name) {
+        return  switch (name) {
+            case "홍길동" -> "INFP";
+            case "홍길순" -> "ENFP";
+            case "임꺽정" -> "ENFJ";
+            case "반장" -> "ESTJ";
+            default -> "모름";
+        };
     }
 }
