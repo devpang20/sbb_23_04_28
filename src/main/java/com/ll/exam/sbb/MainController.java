8fa90169f64107e549cfbf0b14d279680d0a474e
sik2 <devpang20@gmail.com>
2023-05-04 11:37:01 +0900

    redirect

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,44 +2,13 @@ package com.ll.exam.sbb;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    int num = -1;
 
-    @RequestMapping("/plus")
-    @ResponseBody
-    public int showPlus(int a, int b) {
-        return a + b;
-    }
-
-    @RequestMapping("/minus")
-    @ResponseBody
-    public int showMinus(int a, int b) {
-        return a - b;
-    }
-
-    @RequestMapping("/increase")
-    @ResponseBody
-    public int showIncrease() {
-        num++;
-        return num;
-    }
-
-    @RequestMapping("/mbti/{name}")
-    @ResponseBody
-    public String showMbti(@PathVariable  String name) {
-        return  switch (name) {
-            case "홍길동" -> {
-                char j = 'J';
-                yield "INF" + j;
-            }
-            case "홍길순", "임꺽정" -> "ENFJ";
-            case "반장" -> "ESTJ";
-            default -> "모름";
-        };
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
