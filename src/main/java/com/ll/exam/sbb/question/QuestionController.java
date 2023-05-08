5758c24c454f44f124acc0034bd5f9d754246738
sik2 <devpang20@gmail.com>
2023-05-08 13:10:42 +0900

    질문 저장

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -28,5 +26,16 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
